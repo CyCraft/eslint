05a0e00ababfc700460d905631450fd8b6e201db
Luca Ban <mesqueeb@users.noreply.github.com>
2026-09-26 10:40:09 +1200

    Revert "chore: prettier"
    This reverts commit 50d9d6d4ffe6b2608e73238964c7f1b0415bf77d.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,12 @@
 export default {
-  printWidth: 100,
-  tabWidth: 2,
-  singleQuote: true,
-  semi: false,
-  bracketSpacing: true,
-  quoteProps: 'consistent',
-  plugins: [
-    'prettier-plugin-organize-imports',
-    'prettier-plugin-jsdoc', // this needs to be last to work correctly
+  "printWidth": 100,
+  "tabWidth": 2,
+  "singleQuote": true,
+  "semi": false,
+  "bracketSpacing": true,
+  "quoteProps": "consistent",
+  "plugins": [
+    "prettier-plugin-organize-imports",
+    "prettier-plugin-jsdoc", // this needs to be last to work correctly
   ],
 }
