5c32cb512fde12c7e6629213349196d1a28f6d06
Luca Ban <mesqueeb@users.noreply.github.com>
2026-09-26 06:01:25 +1200

    chore: tweak eslint

--- eslint.config.js
+++ eslint.config.js
@@ -12,20 +12,6 @@ export default tseslint.config(
   eslintConfigPrettier,
   {
     rules: {
-      'no-restricted-syntax': [
-        'error',
-        {
-          selector: 'Program > VariableDeclaration > VariableDeclarator > FunctionExpression',
-          message:
-            'Use a function declaration at the top-level instead of arrow functions or function expressions.',
-        },
-        {
-          selector: 'Program > VariableDeclaration > VariableDeclarator > ArrowFunctionExpression',
-          message:
-            'Use a function declaration at the top-level instead of arrow functions or function expressions.',
-        },
-      ],
-      // '@typescript-eslint/no-floating-promises': 'error', // needs parser!
       '@typescript-eslint/explicit-function-return-type': 'error',
       '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
       '@typescript-eslint/consistent-indexed-object-style': ['error', 'index-signature'],
